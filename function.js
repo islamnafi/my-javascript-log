b1f7782cc89a2b8bd3cfcda95d6503975de7423b
Nafi <152697412+islamnafi@users.noreply.github.com>
2025-09-10 19:42:34 +0600

    Initial commit

---
+++ function.js
@@ -0,0 +1,7 @@
+
+function sayHello() {
+    let a = 1;
+    var b = 2;
+}
+
+console.log(b); 
---
+++ intro.js
@@ -0,0 +1,12 @@
+var myName = 'Nafi'; //we use camel case in JavaScript
+let age = 25;
+
+console.log(myName + " " + age);
+
+let personality; //we declared it
+
+console.log(personality); //shows 'undefined' because we have not initialized it yet
+
+const PI = 3.1416;
+
+console.log(PI);
