603e5607731cfbb7aaa25c6dcdb2596674426d61
Nafi <152697412+islamnafi@users.noreply.github.com>
2025-09-12 20:05:52 +0600

    oops, pushed again

---
+++ 2. loops/for-loop.js
@@ -0,0 +1,13 @@
+for(let i=0; i<5; i++){
+    console.log(i);
+}
+
+//console.log(i); -> printing this from outside of the scope of the loop won't work becuase we created the variable i inside that scope.
+
+//but if we used var instead of let
+
+for(var j= 0; j<5; j++){
+    console.log(j);
+}
+
+console.log(j);
